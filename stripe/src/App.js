c79d042514f50a12bff525bd60ec887c478a8f60
yilin18 <yilin18@illinois.edu>
2022-11-19 21:40:46 -0600

    Add Navigation and Router

--- stripe/src/App.js
+++ stripe/src/App.js
@@ -1,36 +1,24 @@
-import logo from './logo.svg';
-import Directory from './Component/directory/directory.component';
+import { Routes, Route } from 'react-router-dom'
+import Home from './router/home/home.component.jsx';
+import Navigation from './router/navigation/navigation.component.jsx';
+
+const Shop = ()=>{
+  return(
+    <div>
+      I am Shop Page.
+    </div>
+  )
+}
 
 function App() {
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-  return (
-    <Directory categories = {categories}/>
+  return(
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path = 'shop' element={<Shop />} />
+      </Route>
+    </Routes>  
+    // <Home/>
   );
 }
 
---
+++ stripe/src/router/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import { ReactComponent as Crwnlogo } from "../../assets/crown.svg";
+import './navigation.styles.scss'
+
+const Navigation = ()=>{
+    return(
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to='/'>
+                    <Crwnlogo/>
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='shop'>
+                        SHOP
+                    </Link>
+                </div>
+            </div>
+            <Outlet></Outlet>
+        </Fragment>
+    )
+}
+
+export default Navigation
